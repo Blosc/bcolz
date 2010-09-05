5601685e250ac1a4adcf8f5a3b76e1242bda5308
Francesc Alted <faltet@antec.pytables.com>
2010-09-05 18:59:18 +0200

    Reverted expression.py benchmark to something shorter.

--- bench/expression.py
+++ bench/expression.py
@@ -8,17 +8,10 @@ import numexpr as ne
 import carray as ca
 from time import time
 
-N = 1e8       # the number of elements in x
+N = 1e7       # the number of elements in x
 clevel = 5    # the compression level
 #sexpr = "(x+1)>0"  # the expression to compute
 sexpr = "(2*x**3+.3*y**2+z+1)<0"  # the expression to compute
-
-# Uncomment the next for disabling threading
-#ne.set_num_threads(1)
-#ca.blosc_set_num_threads(1)
-# Seems that this works better if we dividw the number of cores by 2.
-# Maybe due to some contention between Numexpr and Blosc?
-ca.set_num_threads(ca.ncores//2)
 
 print "Creating inputs..."
 
@@ -35,13 +28,20 @@ else:
 
 print "Evaluating '%s' with 10^%d points" % (sexpr, int(math.log10(N)))
 
-# t0 = time()
-# out = eval(sexpr)
-# print "Time for plain numpy--> %.3f" % (time()-t0,)
+t0 = time()
+out = eval(sexpr)
+print "Time for plain numpy--> %.3f" % (time()-t0,)
 
 t0 = time()
 out = ne.evaluate(sexpr)
 print "Time for numexpr (numpy)--> %.3f" % (time()-t0,)
+
+# Uncomment the next for disabling threading
+#ne.set_num_threads(1)
+#ca.blosc_set_num_threads(1)
+# Seems that this works better if we dividw the number of cores by 2.
+# Maybe due to some contention between Numexpr and Blosc?
+ca.set_num_threads(ca.ncores//2)
 
 t0 = time()
 cout = t.eval(sexpr)
