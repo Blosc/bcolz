425ae970e212736f83754fc3d336c48e1e23b735
Francesc Alted <faltet@antec.pytables.com>
2010-09-05 13:45:04 +0200

    Somewhat improved version of expression benchmark.

--- bench/expression.py
+++ bench/expression.py
@@ -14,8 +14,8 @@ from time import time
 
 N = 1e7       # the number of elements in x
 clevel = 5    # the compression level
-#sexpr = "x+1"  # the expression to compute
-sexpr = "2*x**3+.3*y**2+z+1"  # the expression to compute
+#sexpr = "(x+1)>0"  # the expression to compute
+sexpr = "(2*x**3+.3*y**2+z+1)<0"  # the expression to compute
 
 print "Evaluating '%s' with 10^%d points" % (sexpr, int(math.log10(N)))
 
@@ -23,6 +23,14 @@ print "Evaluating '%s' with 10^%d points" % (sexpr, int(math.log10(N)))
 x = np.arange(N)
 y = np.arange(N)
 z = np.arange(N)
+# Now the compressed arrays
+cx = ca.carray(x, clevel=clevel)
+cy = ca.carray(y, clevel=clevel)
+cz = ca.carray(z, clevel=clevel)
+if 'y' not in sexpr:
+    t = ca.ctable((cx,), names=['x'])
+else:
+    t = ca.ctable((cx, cy, cz), names=['x','y','z'])
 
 t0 = time()
 out = eval(sexpr)
@@ -33,10 +41,6 @@ out = ne.evaluate(sexpr)
 print "Time for numexpr (numpy)--> %.3f" % (time()-t0,)
 
 t0 = time()
-if sexpr == "x+1":
-    t = ca.ctable((x,), names=['x'])
-else:
-    t = ca.ctable((x,y,z), names=['x','y','z'])
 cout = t.eval(sexpr)
 print "Time for ctable--> %.3f" % (time()-t0,)
 print "cout-->", repr(cout)
