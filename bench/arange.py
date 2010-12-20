1549d3ffc2172ac9187da9a571721c4a91148e5d
Francesc Alted <faltet@pytables.org>
2010-12-20 17:25:19 +0100

    Adding a small benchmark on arange.

---
+++ bench/arange.py
@@ -0,0 +1,20 @@
+import numpy as np
+import carray as ca
+from time import time
+
+N = 1e8
+dtype = 'i4'
+
+start, stop, step = 5, N, 4
+
+t0 = time()
+a = np.arange(start, stop, step, dtype=dtype)
+print "Time numpy.arange() --> %.3f" % (time()-t0)
+
+t0 = time()
+ac = ca.arange(start, stop, step, dtype=dtype)
+print "Time carray.arange() --> %.3f" % (time()-t0)
+
+print "ac-->", `ac`
+
+#assert(np.all(a == ac))
