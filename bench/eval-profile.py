588a3d9b5df29d95336c6193ab0e12b7e208d236
Francesc Alted <faltet@pytables.org>
2011-01-24 17:54:23 +0100

    Small modifications for profile benchmark.

--- bench/eval-profile.py
+++ bench/eval-profile.py
@@ -10,10 +10,10 @@ from time import time
 
 def compute_carray(sexpr, clevel, kernel):
     # Uncomment the next for disabling threading
-    ca.set_nthreads(1)
-    ca.blosc_set_nthreads(1)
+    #ca.set_nthreads(1)
+    #ca.blosc_set_nthreads(1)
     print("*** carray (using compression clevel = %d):" % clevel)
-    x = cx
+    x = cx  # comment this for using numpy arrays in inputs
     t0 = time()
     cout = ca.eval(sexpr, kernel=kernel, cparams=ca.cparams(clevel))
     print("Time for ca.eval (%s) --> %.3f" % (kernel, time()-t0,))
@@ -23,22 +23,23 @@ def compute_carray(sexpr, clevel, kernel):
 
 if __name__=="__main__":
 
-    N = 1e7       # the number of elements in x
-    clevel = 0    # the compression level
+    N = 1e8       # the number of elements in x
+    clevel = 9    # the compression level
     sexpr = "(x+1)<0"
     sexpr = "(((.25*x + .75)*x - 1.5)*x - 2)<0"
-    doprofile = 1
+    #sexpr = "(((.25*x + .75)*x - 1.5)*x - 2)"
+    doprofile = 0
 
     print("Creating inputs...")
-    #x = np.arange(N)
-    x = np.linspace(0,100,N)
+    x = np.arange(N)
+    #x = np.linspace(0,100,N)
     cx = ca.carray(x, cparams=ca.cparams(clevel))
 
     print("Evaluating '%s' with 10^%d points" % (sexpr, int(math.log10(N))))
 
-    # t0 = time()
-    # cout = ne.evaluate(sexpr)
-    # print "Time for numexpr --> %.3f" % (time()-t0,)
+    t0 = time()
+    cout = ne.evaluate(sexpr)
+    print "Time for numexpr --> %.3f" % (time()-t0,)
 
     if doprofile:
         import pstats
