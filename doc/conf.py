2560387e8326637dfc3b8a40f7b80f5fcedfaf3b
Francesc Alted <faltet@pytables.org>
2011-02-11 20:16:46 +0100

    Copyright year updated in doc/conf.py.

--- doc/conf.py
+++ doc/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'carray'
-copyright = u'2010, Francesc Alted'
+copyright = u'2010,2011 Francesc Alted'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
