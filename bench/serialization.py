420c0081cb7ae2b8c6c54733c32abbde67b37128
Francesc Alted <francesc@continuum.io>
2012-07-10 12:47:17 +0200

    Added creation time statistics.

--- bench/serialization.py
+++ bench/serialization.py
@@ -2,27 +2,40 @@ import numpy as np
 import carray as ca
 from time import time
 
-N = 1000 * 1000
+N = 1 * 1000 * 1000
 
 a = np.linspace(0, 1, N)
+
+t0 = time()
 ac = ca.carray(a, cparams=ca.cparams(clevel=5))
+print "time creation (memory) ->", round(time()-t0, 3)
+print "data (memory):", repr(ac)
 
-b = ca.carray(a, cparams=ca.cparams(clevel=5), rootdir='proves')
+t0 = time()
+b = ca.carray(a, cparams=ca.cparams(clevel=5), rootdir='myarray')
 b.flush()
-print "meta (memory):", b.read_meta()
-print "data (memory):", sum(b)
+print "time creation (disk) ->", round(time()-t0, 3)
+#print "meta (disk):", b.read_meta()
+print "data (disk):", repr(b)
 
-c = ca.carray(rootdir='proves')
-print "meta (disk):", c.read_meta()
-print "data (disk):", sum(c)
+t0 = time()
+an = np.array(a)
+print "time creation (numpy) ->", round(time()-t0, 3)
+
+t0 = time()
+c = ca.carray(rootdir='myarray')
+print "time open (disk) ->", round(time()-t0, 3)
+#print "meta (disk):", c.read_meta()
+#print "data (disk):", sum(c)
 
 t0 = time()
 sum(ac)
-print "t (memory) ->", round(time()-t0, 3)
+print "time sum (memory) ->", round(time()-t0, 3)
+
 t0 = time()
 sum(c)
-print "t (disk) ->", round(time()-t0, 3)
+print "time sum (disk) ->", round(time()-t0, 3)
 
 t0 = time()
 sum(a)
-print "t (numpy) ->", round(time()-t0, 3)
+print "time sum (numpy) ->", round(time()-t0, 3)
