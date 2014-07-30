ea7945376bf79f345189f6336ec040b03af5b909
Francesc Alted <faltet@gmail.com>
2014-07-30 10:02:55 +0200

    Getting rid of the nose dependency for Python 2.6

--- bcolz/py2help.py
+++ bcolz/py2help.py
@@ -69,38 +69,10 @@ else:
 
 if sys.version_info[:2] >= (2, 7):
     from ctypes import c_ssize_t
-    from unittest import skip, skipIf
 else:
     import ctypes
     if ctypes.sizeof(ctypes.c_void_p) == 4:
         c_ssize_t = ctypes.c_int32
     else:
         c_ssize_t = ctypes.c_int64
-    from nose.plugins.skip import SkipTest
-    class skip(object):
-        def __init__(self, reason):
-            self.reason = reason
-
-        def __call__(self, func):
-            from nose.plugins.skip import SkipTest
-            def wrapped(*args, **kwargs):
-                raise SkipTest("Test %s is skipped because: %s" %
-                                (func.__name__, self.reason))
-            wrapped.__name__ = func.__name__
-            return wrapped
-    class skipIf(object):
-        def __init__(self, condition, reason):
-            self.condition = condition
-            self.reason = reason
-
-        def __call__(self, func):
-            if self.condition:
-                from nose.plugins.skip import SkipTest
-                def wrapped(*args, **kwargs):
-                    raise SkipTest("Test %s is skipped because: %s" %
-                                    (func.__name__, self.reason))
-                wrapped.__name__ = func.__name__
-                return wrapped
-            else:
-                return func
 
