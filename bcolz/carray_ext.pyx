5b6d5347efc28a0b97460ed5c5bcaa88a8561422
Francesc Elies Henar <felies@visualfabriq.com>
2014-10-01 16:04:15 +0200

    ctable: where_terms & groupby + tests

--- bcolz/carray_ext.pyx
+++ bcolz/carray_ext.pyx
@@ -18,7 +18,19 @@ import json
 import datetime
 
 import numpy as np
+cimport numpy as np
 import cython
+
+from cpython.ref cimport PyObject, Py_DECREF, Py_INCREF, Py_XDECREF
+from cpython cimport (PyDict_New, PyDict_GetItem, PyDict_SetItem,
+                      PyDict_Contains, PyDict_Keys,
+                      PyTuple_SET_ITEM,
+                      PyList_Check, PyList_GET_SIZE, PyFloat_Check,
+                      PyString_Check,
+                      PyBytes_Check,
+                      PyTuple_SetItem,
+                      PyTuple_New,
+                      PyObject_SetAttrString)
 
 import bcolz
 from bcolz import utils, attrs, array2string
@@ -2605,6 +2617,107 @@ cdef class carray:
         fullrepr = header + str(self)
         return fullrepr
 
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef carray_is_in(carray col, set value_set, np.ndarray boolarr, bint reverse):
+    """
+    Update a boolean array with checks whether the values of a column (col) are in a set (value_set)
+    Reverse means "not in" functionality
+
+    For the 0d array work around, see https://github.com/Blosc/bcolz/issues/61
+
+    :param col:
+    :param value_set:
+    :param boolarr:
+    :param reverse:
+    :return:
+    """
+    cdef Py_ssize_t i
+    i = 0
+    if not reverse:
+        for val in col.iter():
+            if val not in value_set:
+                boolarr[i] = False
+            i += 1
+    else:
+        for val in col.iter():
+            if val in value_set:
+                boolarr[i] = False
+            i += 1
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef inline object _dict_update(dict d, tuple key, tuple input_val):
+    cdef PyObject *obj = PyDict_GetItem(d, key)
+    cdef tuple current_val, new_val
+    cdef Py_ssize_t i, N
+    if obj is NULL:
+        PyDict_SetItem(d, key, input_val)
+    else:
+        current_val = <object>obj
+        N = PyList_GET_SIZE(input_val)
+        new_val = PyTuple_New(N)
+        for i in range(N):
+            row_val = current_val[i] + input_val[i]
+            Py_INCREF(row_val)
+            PyTuple_SET_ITEM(new_val, i, row_val)
+        PyDict_SetItem(d, key, new_val)
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef groupby_cython(ctable_iter, list groupby_cols, list measure_cols, dict col_dtype_set):
+    """
+    Groups the measure_cols over the groupby_cols. Currently only sums are supported.
+
+    :param groupby_cols: A list of groupby columns
+    :param measure_cols: A list of measure columns (sum only atm)
+    :return:
+    """
+    cdef int actual_len, i, j, col_nr, hash_max, \
+        current_index, previous_index, \
+        groupby_cols_len, measure_cols_len
+    cdef str col
+    cdef list outcols, total_matrix
+    cdef object row
+    cdef bint hash_found
+    cdef np.ndarray hash_arr, output_arr
+    cdef tuple groupby_tup, measure_tup
+    cdef dict total_dict
+    total_dict = {}
+    hash_max = 0
+    current_index = 0
+    outcols = groupby_cols + measure_cols
+    groupby_cols_len = len(groupby_cols)
+    measure_cols_len = len(measure_cols)
+    # process the rows
+    for row in ctable_iter:
+        # lookup values and create hash
+        groupby_tup = row[0:groupby_cols_len]
+        measure_tup = row[groupby_cols_len:]
+        _dict_update(total_dict, groupby_tup, measure_tup)
+    # now create the output table
+    actual_len = len(total_dict)
+    total_matrix = []
+    for col in outcols:
+        output_arr = np.zeros(actual_len, col_dtype_set[col])
+        total_matrix.append(output_arr)
+    # now fill the output table
+    i = 0
+    for groupby_tup, measure_tup in total_dict.iteritems():
+        # save groupby
+        for j in range(groupby_cols_len):
+            total_matrix[j][i] = groupby_tup[j]
+        # save measures
+        for j in range(measure_cols_len):
+            total_matrix[j + groupby_cols_len][i] = measure_tup[j]
+        # prepare for next row
+        i += 1
+    # create the output table
+    output_table = bcolz.ctable(columns=total_matrix, names=outcols)
+    return output_table
 
 
 ## Local Variables:
