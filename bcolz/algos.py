2afa7e5ca99e09b424caa1c07e46ea66e3973265
Valentin Haenel <valentin@haenel.co>
2014-10-07 16:06:30 +0200

    second round of pure-python implementation

--- bcolz/algos.py
+++ bcolz/algos.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from bcolz import carray
+import numpy as np
 
 
 def factorize_pure(carray_):
@@ -15,5 +16,27 @@ def factorize_pure(carray_):
             lookup[element] = idx = count
             count += 1
         labels.append(idx)
+
+    return labels, lookup
+
+
+def factorize_pure2(carray_):
+    count = 0
+    lookup = OrderedDict()
+    n = len(carray_)
+    labels = carray([], dtype='uint64', expectedlen=n)
+
+    buffer = np.empty(len(carray_.chunks[0][:]), dtype='uint64')
+
+    for chunk in carray_.chunks:
+
+        for i, element in enumerate(chunk[:]):
+            try:
+                idx = lookup[element]
+            except KeyError:
+                lookup[element] = idx = count
+                count += 1
+            buffer[i] = idx
+        labels.append(buffer)
 
     return labels, lookup
