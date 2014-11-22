e3e7d1a174ff6b0ebf5fb2b27b2e22a2adf1c2e6
Matthew Rocklin <mrocklin@gmail.com>
2014-11-21 19:38:49 -0800

    add conda recipe

---
+++ conda.recipe/bld.bat
@@ -0,0 +1,16 @@
+
+SET BLD_DIR=%CD%
+cd /D "%RECIPE_DIR%\.."
+
+"%PYTHON%" setup.py install
+
+REM # X.X.X.dev builds
+FOR /F "delims=" %%i IN ('git describe --tags') DO set BCOLZ_VERSION=%%i
+set _result=%BCOLZ_VERSION:-=_%
+
+echo %_result%>__conda_version__.txt
+
+copy __conda_version__.txt "%BLD_DIR%"
+
+if errorlevel 1 exit 1
+
