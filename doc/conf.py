be0753c62f26f1ade94ccddcbc62ffa68e8a58f3
Valentin Haenel <valentin@haenel.co>
2015-01-03 20:40:38 +0100

    update the copyright in the documentation too

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'bcolz'
-copyright = u'2010-2014 Francesc Alted'
+copyright = u'2010-2015 Francesc Alted and the bcolz contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
