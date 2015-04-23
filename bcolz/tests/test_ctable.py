402edac92776d5158b44f5f7299f09506733130e
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-04-23 10:35:20 -0600

    python3 and formatting

--- bcolz/tests/test_ctable.py
+++ bcolz/tests/test_ctable.py
@@ -2084,13 +2084,13 @@ class FlushDiskTest(MayBeDiskTest, TestCase):
     def test_strings(self):
         """Testing that we can add fixed length strings to a ctable"""
         dtype = np.dtype([("a", "|S5"),
-                   ("b", np.uint8),
-                   ("c", np.int32),
-                   ("d", np.float32)])
+                          ("b", np.uint8),
+                          ("c", np.int32),
+                          ("d", np.float32)])
         t = bcolz.ctable(np.empty(0, dtype=dtype), mode="w")
         t.append(("aaaaa", 23, 34567, 1.2355))
         self.assertTrue(len(t) == 1)
-        self.assertTrue(t["a"][0] == "aaaaa")
+        self.assertTrue(t["a"][0] == b"aaaaa", t["a"][0])
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
