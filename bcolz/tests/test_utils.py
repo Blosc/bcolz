26dc8b51beeb9fe45060f765b6461ee488d94b04
santi <sdvillal@gmail.com>
2015-07-13 17:52:41 +0200

    add test for `to_ndarray`

---
+++ bcolz/tests/test_utils.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+########################################################################
+#
+#       License: BSD
+#       Created: July 13, 2015
+#       Author:  Francesc Alted - francesc@blosc.io
+#
+########################################################################
+
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from bcolz.tests.common import TestCase
+from bcolz.utils import to_ndarray
+
+
+class tondarrayTest(TestCase):
+
+    def test_dtype_None(self):
+        array = np.array([[0, 1, 2], [2, 1, 0]]).T
+        assert_array_equal(array, to_ndarray(array, None, safe=True),
+                           'to_ndarray: Non contiguous arrays are not being consolidated when dtype is None')
