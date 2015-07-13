bb858560fe373b2aeb5b8f94de5c7a929ad78611
santi <sdvillal@gmail.com>
2015-07-13 19:10:21 +0200

    fix incorrect test

--- bcolz/tests/test_utils.py
+++ bcolz/tests/test_utils.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from numpy.testing import assert_array_equal
 
 from bcolz.tests.common import TestCase
 from bcolz.utils import to_ndarray
@@ -10,5 +9,5 @@ class tondarrayTest(TestCase):
 
     def test_dtype_None(self):
         array = np.array([[0, 1, 2], [2, 1, 0]]).T
-        assert_array_equal(array, to_ndarray(array, None, safe=True),
-                           'to_ndarray: Non contiguous arrays are not being consolidated when dtype is None')
+        self.assertTrue(to_ndarray(array, None, safe=True).flags.contiguous,
+                        msg='to_ndarray: Non contiguous arrays are not being consolidated when dtype is None')
