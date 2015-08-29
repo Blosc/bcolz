3effdf1fb09fd165c6cc919831fdecaf6fdb64d1
Francesc Elies Henar <felies@visualfabriq.com>
2015-08-29 10:15:36 +0200

    use miniconda latest version
    Conflicts:
    	continuous-integration/travis/install.sh

--- continuous-integration/appveyor/install.ps1
+++ continuous-integration/appveyor/install.ps1
@@ -8,9 +8,9 @@ $MINICONDA_URL = "http://repo.continuum.io/miniconda/"
 function DownloadMiniconda ($python_version, $platform_suffix) {
     $webclient = New-Object System.Net.WebClient
     if ($python_version -match "3.4") {
-        $filename = "Miniconda3-3.5.5-Windows-" + $platform_suffix + ".exe"
+        $filename = "Miniconda3-latest-Windows-" + $platform_suffix + ".exe"
     } else {
-        $filename = "Miniconda-3.5.5-Windows-" + $platform_suffix + ".exe"
+        $filename = "Miniconda-latest-Windows-" + $platform_suffix + ".exe"
     }
     $url = $MINICONDA_URL + $filename
 
