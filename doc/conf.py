d079fc50f7abce0772efc779e5d1ef549252942e
Francesc Alted <francesc@continuum.io>
2015-11-16 12:54:27 +0100

    Changed to alabaster theme for sphinx docs

--- doc/conf.py
+++ doc/conf.py
@@ -104,7 +104,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -128,8 +128,8 @@ html_theme = 'default'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = 'favicon.ico'
-html_favicon = 'favicon-32x32.png'
+html_favicon = 'favicon.ico'
+#html_favicon = 'favicon-32x32.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
