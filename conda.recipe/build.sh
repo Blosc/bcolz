52d5d41dfc957ed12e874cc1e54f2c6eed927210
Stefan Jansen <stefan@applied-ai.com>
2021-04-04 18:11:03 -0500

    install_option

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -2,8 +2,6 @@
 printf "\nConfig\n"
 echo $CONDA_BUILD_SYSROOT
 echo $MACOSX_DEPLOYMENT_TARGET
-export CFLAGS="${CFLAGS} --sysroot=${CONDA_BUILD_SYSROOT}"
-echo $CFLAGS
 printf "\nConfig\n"
 
-python setup.py install -v
+python -m pip install -v --install-option="-Isysroot=${CONDA_BUILD_SYSROOT}" .
