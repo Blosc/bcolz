d978f7a067c6ea2d14240bece84025db2555db60
Stefan Jansen <stefan@applied-ai.com>
2021-04-04 18:26:56 -0500

    retry diff sdk path

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -2,6 +2,8 @@
 printf "\nConfig\n"
 echo $CONDA_BUILD_SYSROOT
 echo $MACOSX_DEPLOYMENT_TARGET
+conda list
+which python
 printf "\nConfig\n"
 
-python -m pip install -v --install-option="-Isysroot=${CONDA_BUILD_SYSROOT}" .
+python -m pip install -vv .
