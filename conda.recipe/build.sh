bf7764d311c5dbb2feb39ce2a61b98d8629f0908
Stefan Jansen <stefan@applied-ai.com>
2021-04-04 17:53:38 -0500

    new build.sh

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+printf "\nConfig\n"
 echo $CONDA_BUILD_SYSROOT
 echo $MACOSX_DEPLOYMENT_TARGET
-export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+export CFLAGS="${CFLAGS} --sysroot=${CONDA_BUILD_SYSROOT}"
 echo $CFLAGS
+printf "\nConfig\n"
 
 python setup.py install -v
